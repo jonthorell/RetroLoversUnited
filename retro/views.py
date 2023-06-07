12c386c6ef23ce7adfe9b68a8b122c02eac43054
jonthorell <jthorell@gmail.com>
2023-06-07 22:00:14 +0200

    Change base.html to more baseline to start with. Add functionality to have to keep changing debug= from true/false in development and production respectively

--- retro/views.py
+++ retro/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic, View
+
+
 
 # Create your views here.
+
+class home(generic.ListView):
+    template_name = "index.html"
--- retroloversunited/urls.py
+++ retroloversunited/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+import retro
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #path('', include('retro.urls'), name='retro_urls'),
+    #path('', retro.urls),
 ]
