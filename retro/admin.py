d69cb3f9776b72fd011a6ce8fa913345f3565774
jonthorell <jthorell@gmail.com>
2023-06-10 23:02:33 +0200

    Update: models updated

--- retro/admin.py
+++ retro/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Category,Article,Links
+from .models import Category,Article,Link,Comment
 
 # Register your models here.
 
 admin.site.register(Category)
 admin.site.register(Article)
-admin.site.register(Links)
+admin.site.register(Link)
+admin.site.register(Comment)
--- retro/models.py
+++ retro/models.py
@@ -1,29 +1,60 @@
 # from getpass import getuser
 from django.db import models
 from django.contrib.auth import get_user_model
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+# status is used to check wheter comment or article has been approved
+
+STATUS = ((0, "Draft"), (1,"Published"))
 
 User=get_user_model()
 
 class Category(models.Model):
-    name = models.CharField(max_length=40, blank=False, null=False)
+    name = models.CharField(max_length=40, blank=False, null=False, unique=True)
     def __str__ (self):
         return self.name
 
 class Article(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title = models.CharField(max_length=60, blank=False, null=False)
+    slug = models.SlugField(max_length=200, unique=True)
+    title = models.CharField(max_length=60, blank=False, null=False, unique=True)
     content = models.TextField(max_length=2000, blank=False, null=False)
+    updated_on = models.DateTimeField(auto_now=True)
+    featured_image = CloudinaryField('image', default='placeholder')
+    created_on = models.DateTimeField(auto_now_add=True)
+    excerpt = models.TextField(blank=True)
+    status = models.IntegerField(choices=STATUS, default=0)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-created_on']
 
     def __str__(self):
         return self.title
 
-class Links(models.Model):
-    name = models.CharField(max_length=60, blank=False, null=False)
-    url = models.CharField(max_length=255, blank=False, null=False)
+class Link(models.Model):
+    name = models.CharField(max_length=60, blank=False, null=False, unique=True)
+    url = models.CharField(max_length=255, blank=False, null=False, unique=True)
+    description = models.CharField(max_length=255, blank=False, null=False)
     alt = models.CharField(max_length=255, blank=False, null=False)
 
     def __str__(self):
         return self.name
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
+
