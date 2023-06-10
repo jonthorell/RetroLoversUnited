95def36330c3f274ab7058996962c4819003e267
jonthorell <jthorell@gmail.com>
2023-06-11 01:46:15 +0200

    Add: All links in navbar are redirecting to a proper template. The ones under Categories to a generic dummy one so far since the menu is not yet retrieving t he categogy names from the database.

--- retro/urls.py
+++ retro/urls.py
@@ -1,6 +1,11 @@
+from unicodedata import category
 from django.urls import path
-from .views import Index
+from .views import Index, Links,Contact, About, Category
 
 urlpatterns = [
-    path('', Index.as_view(), name='home')
+    path('', Index.as_view(), name='home'),
+    path('links', Links.as_view(), name='links'),
+    path('contact',Contact.as_view(), name='contact'),
+    path('about', About.as_view(), name='about'),
+    path('category', Category.as_view(), name='Category')
     ]
--- retro/views.py
+++ retro/views.py
@@ -1,4 +1,19 @@
+from django.shortcuts import render, get_object_or_404, reverse
+from django.views import generic, View
+from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView
 
 class Index(TemplateView):
     template_name = 'retro/index.html'
+
+class Links(TemplateView):
+    template_name = 'retro/links.html'
+
+class Contact(TemplateView):
+    template_name = 'retro/contact.html'
+
+class About(TemplateView):
+    template_name = 'retro/about.html'
+    
+class Category(TemplateView):
+    template_name = 'retro/category.html'
