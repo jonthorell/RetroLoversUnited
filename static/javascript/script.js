35d03f2059db8527fdb16657427ffb03a2d15820
jonthorell <jthorell@gmail.com>
2023-06-26 16:15:35 +0200

    Update: updated font awsome to 6.0 and Materialize Bootstrap to 6.4. Add: datepicker to create_article

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -3,3 +3,8 @@
 const welcomePhrase = "Another visitor!\nStay a while, stay forever!";
 console.log(welcomePhrase)
 
+const datepickerDisablePast = document.querySelector('.datepicker-disable-past');
+new mdb.Datepicker(datepickerDisablePast, {
+    disablePast: true
+});
+
