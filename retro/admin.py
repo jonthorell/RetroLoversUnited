545105a3e10cbdc4f3e42dabba1b43e7115cbcc5
jonthorell <jthorell@gmail.com>
2023-07-18 03:43:45 +0200

    Add: further customization of the pagination. Border added around the next-previous div. URL to links.json removed. Not necessary anymore.

--- retro/admin.py
+++ retro/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
+from django.contrib.auth.models import User, Group
 from .models import Category,Article,Link,Comment,Profile
 
 class ArticleAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
 
+#class ProfileInline(admin.StackedInline):
+#    model = Profile
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    #inlines = [ProfileInline]
+
 # Register your models here.
 
+admin.site.unregister(User)
+admin.site.unregister(Group)
+admin.site.register(User, UserAdmin)
 admin.site.register(Category)
 admin.site.register(Article,ArticleAdmin)
 admin.site.register(Link)
