b71d941f2d684ac25033e1c5b534cee9a40ef79e
jonthorell <jthorell@gmail.com>
2023-07-20 22:28:23 +0200

    Update: changed script.js to make the alert look a bit nicer

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,7 +1,8 @@
 //start with defining some constants
 
 function printAlert(newMessage) {
-    let myAlert = "<h2>" + newMessage + "</h2><hr>"
+    let myAlert = '<h2><img src="https://res.cloudinary.com/retroloversunited/raw/upload/v1686394021/static/images/Boing_Ball.5037f34b676b.svg" height="40" width="40" alt="BoingBall logo" loading="lazy">'
+    myAlert += "    " + newMessage + "</h2><hr>"
     console.log(myAlert)
     document.getElementById('alert-id').innerHTML = myAlert;
     mdb.Alert.getInstance(document.getElementById('alert-id')).show();
