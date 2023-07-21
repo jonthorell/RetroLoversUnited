dd9a26f78c5d0408b61f7e588fcc3f3bd3cba060
jonthorell <jthorell@gmail.com>
2023-07-21 23:38:48 +0200

    Add: all functions moved to utils.py and everything still works. There's one "standalone" function remaining and that one shall be refactored into a class.

--- retro/utils.py
+++ retro/utils.py
@@ -1,2 +1,76 @@
+from django.core.paginator import Paginator
+from retro.models import Link, Article, Category, Comment,User,Profile
+from django.http import Http404, HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404,render,redirect
+from django.core.exceptions import PermissionDenied
+
 def multiply(a, b): 
     return a * b 
+
+def listing_links(request, page):
+    keywords = Link.objects.all().order_by("name")
+    paginator = Paginator(keywords, per_page=5)
+    page_object = paginator.get_page(page)
+    context = {"page_obj": page_object}
+    return render(request, "retro/links.html", context)
+
+def listing_links_api(request):
+    page_number = request.GET.get("page", 1)
+    per_page = request.GET.get("per_page", 5)
+    startswith = request.GET.get("startswith", "")
+    keywords = Link.objects.filter(
+        name__startswith=startswith
+    )
+    paginator = Paginator(keywords, per_page)
+    page_obj = paginator.get_page(page_number)
+    data = [{"name": kw.name} for kw in page_obj.object_list]
+
+    payload = {
+        "page": {
+            "current": page_obj.number,
+            "has_next": page_obj.has_next(),
+            "has_previous": page_obj.has_previous(),
+        },
+        "data": data
+    }
+    return JsonResponse(payload)
+
+def listing_article_api(request):
+    
+    page_number = request.GET.get("page", 1)
+    per_page = request.GET.get("per_page", 5)
+    #startswith = request.GET.get("startswith", "")
+    keywords = Article.objects.filter(category_id=15)
+        
+    paginator = Paginator(keywords, per_page)
+    page_obj = paginator.get_page(page_number)
+    data = [{"title": kw.title} for kw in page_obj.object_list]
+
+    payload = {
+        "page": {
+            "current": page_obj.number,
+            "has_next": page_obj.has_next(),
+            "has_previous": page_obj.has_previous(),
+        },
+        "data": data
+    }
+    return JsonResponse(payload)
+
+def listing_article_by_category(request, page):
+    keywords = Article.objects.all().order_by("name")
+    paginator = Paginator(keywords, per_page=5)
+    page_object = paginator.get_page(page)
+    context = {"page_obj": page_object}
+    return render(request, "retro/articles_by_category.html", context)
+
+def check_user_able_to_see_page(*groups):
+
+    def decorator(function):
+        def wrapper(request, *args, **kwargs):
+            if request.user.groups.filter(name__in=groups).exists():
+                return function(request, *args, **kwargs)
+            raise PermissionDenied
+
+        return wrapper
+
+    return decorator
--- retro/views.py
+++ retro/views.py
@@ -11,68 +11,12 @@ from retro.models import Link, Article, Category, Comment,User,Profile
 from django.http import Http404, HttpResponse, JsonResponse
 from django.core.exceptions import PermissionDenied
 from django.core.paginator import Paginator
-from .utils import multiply
+from .utils import multiply, listing_links, listing_links_api, listing_article_api,listing_article_by_category, check_user_able_to_see_page
 
 def my_view(request): 
     # Temporary to test custom utils.py
     result = multiply(2, 3) 
     return HttpResponse(result) 
-
-def listing_links(request, page):
-    keywords = Link.objects.all().order_by("name")
-    paginator = Paginator(keywords, per_page=5)
-    page_object = paginator.get_page(page)
-    context = {"page_obj": page_object}
-    return render(request, "retro/links.html", context)
-
-def listing_links_api(request):
-    page_number = request.GET.get("page", 1)
-    per_page = request.GET.get("per_page", 5)
-    startswith = request.GET.get("startswith", "")
-    keywords = Link.objects.filter(
-        name__startswith=startswith
-    )
-    paginator = Paginator(keywords, per_page)
-    page_obj = paginator.get_page(page_number)
-    data = [{"name": kw.name} for kw in page_obj.object_list]
-
-    payload = {
-        "page": {
-            "current": page_obj.number,
-            "has_next": page_obj.has_next(),
-            "has_previous": page_obj.has_previous(),
-        },
-        "data": data
-    }
-    return JsonResponse(payload)
-
-def listing_article_api(request):
-    
-    page_number = request.GET.get("page", 1)
-    per_page = request.GET.get("per_page", 5)
-    #startswith = request.GET.get("startswith", "")
-    keywords = Article.objects.filter(category_id=15)
-        
-    paginator = Paginator(keywords, per_page)
-    page_obj = paginator.get_page(page_number)
-    data = [{"title": kw.title} for kw in page_obj.object_list]
-
-    payload = {
-        "page": {
-            "current": page_obj.number,
-            "has_next": page_obj.has_next(),
-            "has_previous": page_obj.has_previous(),
-        },
-        "data": data
-    }
-    return JsonResponse(payload)
-
-def listing_article_by_category(request, page):
-    keywords = Article.objects.all().order_by("name")
-    paginator = Paginator(keywords, per_page=5)
-    page_object = paginator.get_page(page)
-    context = {"page_obj": page_object}
-    return render(request, "retro/articles_by_category.html", context)
 
 class EditorRequiredMixin(UserPassesTestMixin):
     # Class used to restrict access to views where user needs to be editor
@@ -93,18 +37,6 @@ class ManagerRequiredMixin(UserPassesTestMixin):
     # Class used to restrict access to views where user needs to be manager
     def test_func(self):
         return self.request.user.groups.filter(name="Managers").exists()
-
-def check_user_able_to_see_page(*groups):
-
-    def decorator(function):
-        def wrapper(request, *args, **kwargs):
-            if request.user.groups.filter(name__in=groups).exists():
-                return function(request, *args, **kwargs)
-            raise PermissionDenied
-
-        return wrapper
-
-    return decorator
 
 class custom_mixin_kategorimenu(object):
     def get_context_data(self, **kwargs):
@@ -170,8 +102,6 @@ class Index(custom_mixin_kategorimenu, TemplateView):
 
         return context
 
-
-
 class Contact(custom_mixin_kategorimenu, TemplateView):
     template_name = 'retro/contact.html'
 
@@ -181,18 +111,13 @@ class FAQ(custom_mixin_kategorimenu, TemplateView):
 class Credits(custom_mixin_kategorimenu, TemplateView):
     template_name = 'credits/credits.html'
 
-
-
-
 class About(custom_mixin_kategorimenu, TemplateView):
     template_name = 'retro/about.html'
-
     
 class Kategories(custom_mixin_kategorimenu, ListView):
     template_name = 'retro/category.html'
     model = Category
     context_object_name = 'categories'
-
 
 class Edit_profile(MemberRequiredMixin,custom_mixin_kategorimenu, TemplateView):
     template_name = 'retro/edit_profile.html'
@@ -210,10 +135,8 @@ class View_profile(MemberRequiredMixin, custom_mixin_kategorimenu, TemplateView)
 class Test(AdminRequiredMixin, custom_mixin_kategorimenu, TemplateView):
     template_name = 'retro/test.html'
 
-    
 class List_Users(ManagerRequiredMixin, custom_mixin_kategorimenu, TemplateView):
     template_name = 'retro/list_users.html'    
-
 
 class Links(custom_mixin_kategorimenu, ListView):
     template_name = 'retro/links.html'
